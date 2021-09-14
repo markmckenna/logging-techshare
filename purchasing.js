74c27529cfab04ed50e295b1c15f930d6c8ef9fa
Mark McKenna <mark.mckenna@redspace.com>
2021-09-14 18:36:49 -0300

    Closure-based logging

--- purchasing.js
+++ purchasing.js
@@ -1,13 +1,7 @@
 function buySomething(customer, product, quantity) {
-    let log = log.start(`${customer} purchasing ${quantity} units of ${product}`)
-
-    try {
+    log.inContext(`${customer} purchasing ${quantity} units of ${product}`, () => {
         inventory.reserve(product, quantity)
         customer.shoppingCart.add(product, quantity)
         customer.notifyUpdates()
-    } catch (err) {
-        log.failWith(err)
-    }
-
-    log.complete()
+    })
 }
