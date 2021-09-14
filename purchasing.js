143f57a9ee14ed51964212a61869eec6b583fe12
Mark McKenna <mark.mckenna@redspace.com>
2021-09-14 18:27:26 -0300

    Naive, debugger-based logging

--- purchasing.js
+++ purchasing.js
@@ -1,5 +1,7 @@
 function buySomething(customer, product, quantity) {
+    console.debug('buySomething started', customer, product, quantity)
     inventory.reserve(product, quantity)
     customer.shoppingCart.add(product, quantity)
     customer.notifyUpdates()
+    console.debug('buySomething done', customer, product, quantity)
 }
